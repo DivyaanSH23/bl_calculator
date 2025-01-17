255871138119c385d0fee20eb3ff81488946b696
DivyaaSH23 <divyanshshuklaofficial23@gmail.com>
2025-01-17 11:55:25 +0530

    intial

---
+++ src/addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class addition {
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("ENTER TWO NUMBERS FOR ADDITION");
+        int i=sc.nextInt();
+        int j=sc.nextInt();
+        int z=i+j;
+        System.out.println("THE RESILT IS : "+z);
+
+    }
+
+}
